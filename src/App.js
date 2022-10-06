e57746ddf95b79311fdd1adb46e45f3c70cafff0
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-07 01:19:04 +0600

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import About from './components/About/About';
 import Shop from './components/Shop/Shop';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>,
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>,
+        },
+        {
+          path: '/inventory',
+          element: <Inventory></Inventory>,
+        },
+        {
+          path: '/about',
+          element: <About></About>,
+        }
+      ]
+    }
+  ]);
+
+
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router} />
     </div>
   );
 }
