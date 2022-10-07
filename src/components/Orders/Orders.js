7622128f36281287cf8d06ec06b0c6578dc4b828
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-08 01:15:34 +0600

    Review item and font awesome delete style

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const { products, previousCart } = useLoaderData();    //{ products, previousCart }
@@ -8,14 +9,13 @@ const Orders = () => {
     return (
         <div>
             <div className='shop-container'>
-                <div className="products-container">
-                    {/* {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handlerAddToCart={handlerAddToCart}
-                    ></Product>)
-                } */}
+                <div className="order-container">
+                    {
+                        cart.map(product => <ReviewItem
+                            key={product.id}
+                            product={product}
+                        ></ReviewItem>)
+                    }
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}></Cart>
