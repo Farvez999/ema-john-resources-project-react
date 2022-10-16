ef06a91c24a886301a7647a844d662f47d083be5
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-17 00:42:32 +0600

    Get Form Data and display form error

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './SignUp.css'
 
 const SignUp = () => {
+
+    const [error, setError] = useState(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm)
+
+        if (password.length < 6) {
+            setError('Password must be 10 charters long')
+            return;
+        }
+        if (password !== confirm) {
+            setError('Your password is did not match')
+            return;
+        }
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="" required />
@@ -18,9 +40,10 @@ const SignUp = () => {
                     <label htmlFor="confirm">Confirm Password</label>
                     <input type="password" name="confirm" id="" required />
                 </div>
-                <input className='btn-submit' type="submit" value="Login" />
+                <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p>Already have a Account <Link to='/login'>Log In</Link></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
