09b9a3acb2a5c35bf072c51995ba481a62272d50
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-17 00:16:35 +0600

    Setup Auth Context under UserContext to share auth info

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" name="confirm" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>Already have a Account <Link to='/login'>Log In</Link></p>
         </div>
     );
 };
