749c0be5e3c8d72919acf8e98ad9a38310308e6c
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-10-17 01:32:05 +0600

    Implement Login Sign Up and Sign Out

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/UserContext';
 import './SignUp.css'
 
 const SignUp = () => {
 
     const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext)
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -22,6 +24,16 @@ const SignUp = () => {
             setError('Your password is did not match')
             return;
         }
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(error => {
+                console.error(error)
+            })
     }
 
     return (
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,13 +1,28 @@
 import React, { createContext, useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebaseConfig'
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
+
     const [user, setUser] = useState(null);
-    const authInfo = { user }
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    const authInfo = { user, createUser, logInUser, logout }
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
